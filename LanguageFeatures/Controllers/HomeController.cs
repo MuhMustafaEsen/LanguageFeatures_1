f58b4b679632b82e7245fc0078ace01672247104
mustafa esen <esenmustafa42@gmail.com>
2019-07-24 19:33:05 +0300

    MVC Core Essential C#

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LanguageFeatures.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+
+            foreach (Product item in Product.GetProducts())
+            {
+                string name = item?.Name ?? "<No Name>";
+                decimal? price = item?.Price ?? 0;
+                string category = item?.Cartegory ?? "<No Category>";
+                
+                if (item?.InStock==true)
+                {
+                    results.Add(string.Format("Name : {0},Category :{1}, Price : {2}", name, category, price));
+                }
+                
+            }
+
+
+            return View(results);
+        }
+    }
+}
---
+++ LanguageFeatures/Model/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Model
+{
+    public class Product
+    {
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
+        public string Name { get; set; }
+        public string Cartegory { get; set; } = "WaterSports"; //başlangıç degeri her zaman bu atanır. 
+        public decimal? Price { get; set; }
+        public bool InStock { get; }
+
+        public static Product[] GetProducts()
+        {
+
+            Product kayak = new Product
+            {
+                Name = "Kayak",
+                Cartegory = "Water Craft", //category propertisine atanan başlangıç degeri degiştirileblir. 
+                Price = 275M
+
+            };
+            Product lifejacket = new Product(false)
+            {
+                Name = "Lifejacket",
+                Price = 48.95M
+            };
+
+            return new Product[] { kayak, lifejacket, null };
+        }
+    }
+}
